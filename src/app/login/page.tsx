97e132ef5ef28f245f2c8a1ccca7fc9e2dba025f
Svarium <svariumfoo@gmail.com>
2024-05-31 18:10:45 -0300

    validaciones con yup

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,5 +1,12 @@
+import LoginForm from "@/components/auth/LoginForm";
+
 const LoginPage = () => {
-    return <div>Pagina inicio de sesion</div>
+    return <div className="flex items-center w-full flex-col">
+
+        <h2 className="mb-4">Iniciar Sesión en la red Social:</h2>
+
+        <LoginForm/>
+    </div>
 }
 
 export default LoginPage;
