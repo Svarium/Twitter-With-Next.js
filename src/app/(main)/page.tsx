ca1191487c8b9063d2534f44cea6438ffbbc12c8
Svarium <svariumfoo@gmail.com>
2024-05-31 13:43:19 -0300

    introduccion a React Hook Form

--- src/app/(main)/page.tsx
+++ src/app/(main)/page.tsx
@@ -1,4 +1,5 @@
 import MessageFeed from "@/components/messages/MessageFeed";
+import MessagePostForm from "@/components/messages/MessagePostForm";
 import messageApi from "@/services/messages/messages.service";
 
 const IndexPage = async () => {
@@ -9,7 +10,8 @@ const IndexPage = async () => {
   return <>
     <main className="flex flex-col bg-gray-100 p-8">
 
-      <section className="flex flex-col mb-8">              
+      <section className="flex flex-col mb-8">  
+          <MessagePostForm />           
           <MessageFeed initialMessages={messageResponse}  />
       </section>
     </main>
---
+++ src/components/messages/MessagePostForm.tsx
@@ -0,0 +1,60 @@
+ 'use client'
+
+import messageApi from "@/services/messages/messages.service";
+import Image from "next/image";
+import { useEffect } from "react";
+import { useForm } from "react-hook-form";
+
+type FormData = {
+    message:string
+}
+
+
+const MessagePostForm = () => {  
+     
+    const {register, handleSubmit, resetField, setFocus} = useForm<FormData>();
+
+    useEffect(() => {
+        setFocus('message')
+    }, [])
+
+    const onSubmit = async (data:FormData) => {
+    const response = await messageApi.postMessage(data.message);
+    resetField("message")  
+    setFocus("message")
+    }
+    
+    return <div  className="mb-4 grid grid-cols-12">
+    <div className="w-full h-full mt-1 text-center mb-4 col-span-2 flex items-center justify-center">
+        <Image
+            className="rounded-full"
+            src={"https://i.pinimg.com/564x/62/ce/55/62ce5561877ab6a4587a2b7dedd4c5ca.jpg"}
+            width={60}
+            height={60}
+            priority
+            alt={""}
+        />
+    </div>
+    <div className="flex flex-col ml-4 mt-2 col-span-10">
+    <form onSubmit={handleSubmit(onSubmit)}>
+     <textarea 
+     rows={4} 
+     className="mb-4 p-4 rounded bg-gray-50 w-full border border-gray-200 resize-none"
+     placeholder="¿Qué estas pensando?"   
+     {...register("message", {
+        required:true
+     })}
+      />   
+      <div className="flex justify-end">
+    <button type="submit" className="button-primary  font-semibold w-fit">Postear</button>      
+      </div>
+      </form>
+    </div>
+</div>
+
+      
+           
+   
+}
+
+export default MessagePostForm;
